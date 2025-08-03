622095c588dc057c1c64c9d0a77803f246138069
Sylvie Fox <sylvie@tycrek.com>
2025-08-02 21:41:23 -0600

    feat: tittycrypto

--- src/main/java/com/tycrek/Main.java
+++ src/main/java/com/tycrek/Main.java
@@ -1,7 +1,13 @@
 package com.tycrek;
 
+import io.ttrms.crypt.TittyCryptoException;
+
+import java.util.Arrays;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TittyCryptoException {
         System.out.println("init");
+        System.out.println(Arrays.toString(TitCrypt.getAvailableEncryptAlgos()));
+        TitCrypt.sampleCryptTest();
     }
 }
---
+++ src/main/java/com/tycrek/TitCrypt.java
@@ -0,0 +1,56 @@
+package com.tycrek;
+
+import io.ttrms.crypt.*;
+
+import java.security.Provider;
+import java.security.Security;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class TitCrypt {
+
+    /**
+     * Shows sample usage for {@link TitCrypt}
+     */
+    public static void sampleCryptTest() throws TittyCryptoException {
+        String sensitiveInformation = "nuclear-launch-codes";
+        String password = "HighlySecure123!";
+
+        CryptSpec cryptSpec = new CryptSpec(
+                "AES_256/GCM/NoPadding",
+                "PBKDF2WithHmacSHA256",
+                16,
+                12,
+                128,
+                250_420,
+                256);
+
+
+        Crypt crypt = new Crypt(cryptSpec, password);
+
+        String encrypted = crypt.encrypt(sensitiveInformation);
+        System.out.println(encrypted);
+
+        String decrypted = crypt.decrypt(encrypted);
+        System.out.println(decrypted);
+
+        /*for (String algo : getAvailableEncryptAlgos())
+            System.out.println(algo);*/
+    }
+
+    /**
+     * Return an array of possible argument values for {@link CryptSpec} <code>encryptAlgo</code>
+     *
+     * @see <a href="https://stackoverflow.com/a/56428353/9665770">How can I list the available Cipher algorithms? (StackOverflow)</a>
+     */
+    @SuppressWarnings("unused")
+    public static String[] getAvailableEncryptAlgos() {
+        Set<String> algos = new TreeSet<>();
+        for (Provider provider : Security.getProviders())
+            provider.getServices().stream()
+                    .filter(s -> "Cipher".equals(s.getType()))
+                    .map(Provider.Service::getAlgorithm)
+                    .forEach(algos::add);
+        return algos.toArray(new String[]{});
+    }
+}
